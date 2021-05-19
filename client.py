d95e9952d346e89c9e2de6ff36c3a7e85b2efc0a
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2021-05-19 15:03:45 +0200

    removed unnesecary imports and changed if logic

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
-import socket, threading, ast, json
+import socket
+import json
 import server, accountancy, wallet_json_rpc
-from log_module import *
+from log_module import logger
 from params import *
 
 last_miner_notify_flag = True
@@ -32,7 +33,7 @@ def client_handler():
             print(e)
             wallet_ok = False
 
-        while wallet_ok == True:
+        while wallet_ok is True:
             try:
                 data = cli.recv(buffer)
             except Exception as e:
@@ -89,7 +90,7 @@ def mining_submit_handler(submit_msg, extranonce):
     #print("Pow to wallet: " + str(msg))
     #print("Wallet ok:   " + str(wallet_json_rpc.wallet_ok))
 
-    if wallet_json_rpc.wallet_ok == True:
+    if wallet_json_rpc.wallet_ok is True:
         cli.sendall(msg.encode())
         print("Block pow sent to wallet:")
         print(msg.encode())
