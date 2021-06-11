2db1082b528973d508ff6aa7c964ad9c06d59062
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2021-06-11 15:18:17 +0200

    Update restapi.py

--- restapi.py
+++ restapi.py
@@ -6,21 +6,6 @@ import sqlite_handler
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
 @app.route('/pool_data', methods=['GET'])
 def get_pool_data():
     pool_data = {
