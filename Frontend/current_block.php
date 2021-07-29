d47bc8aac84e5fddcba6d728dd89ced84f2c08ea
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2021-07-29 12:34:06 +0200

    added net hr and current block

---
+++ Frontend/current_block.php
@@ -0,0 +1,15 @@
+<?php
+$url = "localhost:3000/pool_data";
+
+$ch = curl_init();
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=json_decode(curl_exec($ch), true);
+// Closing
+curl_close($ch);
+
+echo $result['pool_data']['current_block'];
+?>
