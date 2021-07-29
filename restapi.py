01f79cb35377d271ebdbe4b36dbf48b73d67abee
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2021-07-29 12:35:41 +0200

    nethr, current block

--- restapi.py
+++ restapi.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify
 
 from params import pool_fee
+import wallet_json_rpc
 import mining
 import sqlite_handler
 
@@ -9,7 +10,10 @@ app = Flask(__name__)
 
 @app.route('/pool_data', methods=['GET'])
 def get_pool_data():
+    network_height = wallet_json_rpc.get_current_block()
     pool_data = {
+        "current_block": str(network_height),
+        "net_hashrate": str(wallet_json_rpc.get_net_hashrate(network_height)) + " Gh",
         "algorithm": "Pascal",
         "poolhash": str(round(mining.get_pool_hr() / 10**9, 2)) + " Gh",
         "nethash":  0,
