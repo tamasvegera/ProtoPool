6f2423d4a01ca096a3a55c44fd542dd91d8cff79
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2022-01-20 12:55:50 -0800

    fixed logging

--- log_module.py
+++ log_module.py
@@ -1,7 +1,8 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 logger = logging.getLogger("protopool")
-hdlr = logging.TimedRotatingFileHandler('./protopool.log', when='midnight')
+hdlr = logging.handlers.TimedRotatingFileHandler('./protopool.log', when='midnight')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
