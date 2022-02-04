073cdeca9d14d61e7bda085fe22d2f8e33c92c6f
Vegera Tamás <tamasvegera@gmail.com>
2022-02-04 16:00:01 +0100

    fixed request account feature on frontend

--- Frontend/request_account.php
+++ Frontend/request_account.php
@@ -1,23 +1,31 @@
 <?php
-$pubkey = $_POST["pubkey"];
 
-$post = [
-    'pubkey' => $pubkey
-];
+// API URL
+$url = 'localhost:3000/get_account';
 
-$url = "localhost:3000/get_account";
+// Create a new cURL resource
+$ch = curl_init($url);
 
-$ch = curl_init();
-// Will return the response, if false it print the response
+// Setup request to send json via POST
+$data = array(
+    'pubkey' => $_POST["pubkey"]
+);
+$payload = json_encode($data);
+
+// Attach encoded JSON string to the POST fields
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+// Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+// Return response instead of outputting
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-// Execute
-$result=json_decode(curl_exec($ch), true);
-// Closing
-curl_close($ch);
 
+// Execute the POST request
+$result=json_decode(curl_exec($ch), true);
+
+// Close cURL resource
+curl_close($ch);
 echo $result['result'];
+
 ?>
